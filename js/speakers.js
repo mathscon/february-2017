7157c73e24719ce08ea1f2683da2a5b38ff3f148
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2016-08-30 21:01:50 +0100

    Add speaker carousel and fix navbar

---
+++ js/speakers.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ 
+  var owl = $("#speakers-owl");
+ 
+  owl.owlCarousel({
+      items : 4, //10 items above 1000px browser width
+      itemsDesktop : [1000,4], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,4], // betweem 900px and 601px
+      itemsTablet: [600,2], //2 items between 600 and 0
+      itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  });
+
+owl.trigger('owl.play',5000);
+ 
+});
