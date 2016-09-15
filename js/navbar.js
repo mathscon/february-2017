5926fdf82f0d27ec94e0b8da1b8c92ef707dbf5a
Abhinav Mishra <AMishra12@slb.com>
2016-09-15 15:09:52 +0100

    Fix navbar background

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,40 @@
 $(document).ready(function(){       
    var scroll_start = 0;
-   var startchange = $(window).width();
-   if (startchange > 800){
+   var collapsed = true;
    $(document).scroll(function() { 
+   var startchange = $(window).width();
       scroll_start = $(this).scrollTop();
-      if(scroll_start > 75) {
-          $(".navbar").css('background-color', '#171414');
+      if(scroll_start > 0) {
+          navbarGrey();
        } else {
-          $('.navbar').css('background-color', 'transparent');
+          navbarTransparent();
        }
    });
+
+   $(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
     }
+   });
+
+   $(document).on('click','#collapse',function(e) {
+     if($(this).scrollTop() == 0) {
+       if($('.navbar-collapse').is(':visible')) {
+	  navbarGrey();
+       } else {
+          navbarTransparent();
+       }
+     } else {
+	  navbarGrey();
+     }
+   });
+
 });
+
+var navbarTransparent = function() {
+  $('.navbar').css('background-color', 'transparent');
+}
+
+var navbarGrey = function() {
+  $(".navbar").css('background-color', '#171414');
+}
